b668a87acb341ab3fcd7497f1f03a12c9d1ff164
Amirametwally <amira.metwally56@gmail.com>
2024-01-20 02:47:02 +0200

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#Using Puppet, install flask from pip3
+#  install flask from pip3
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
