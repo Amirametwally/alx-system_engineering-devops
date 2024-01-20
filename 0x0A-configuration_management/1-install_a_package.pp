781dfa41955eb176883744318581a3ea418c18a9
Amirametwally <amira.metwally56@gmail.com>
2024-01-20 03:05:12 +0200

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 # Install flask from pip3
 
-package { 'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
