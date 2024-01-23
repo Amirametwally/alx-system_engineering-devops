5b55c550b18111b0fb84bc8ff698e6849ff552e3
Amirametwally <amira.metwally56@gmail.com>
2024-01-24 00:03:05 +0200

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,34 +4,41 @@ exec { 'add nginx stable repo':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
+# update software packages list
 exec { 'update packages':
   command => 'apt-get update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
+# install nginx
 package { 'nginx':
   ensure     => 'installed',
 }
 
+# allow HTTP
 exec { 'allow HTTP':
   command => "ufw allow 'Nginx HTTP'",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
 }
 
+# change folder rights
 exec { 'chmod www folder':
   command => 'chmod -R 755 /var/www',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
+# create index file
 file { '/var/www/html/index.html':
   content => "Hello World!\n",
 }
 
+# create index file
 file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
 }
 
+# add redirection and error page
 file { 'Nginx default config file':
   ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
@@ -65,6 +72,7 @@ exec { 'restart service':
   path    => '/usr/bin:/usr/sbin:/bin',
 }
 
+# start service nginx
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
