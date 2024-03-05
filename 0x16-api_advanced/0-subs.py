27ebe7f9e80966c4e549e02f2567ca021061e779
Amirametwally <amira.metwally56@gmail.com>
2024-03-05 22:54:57 +0200

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API
     """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     response = requests.get(url, headers=headers)
     if not response.ok:
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
+    subscriber_count = response.json().get("data").get("subscribers")
     return subscriber_count
