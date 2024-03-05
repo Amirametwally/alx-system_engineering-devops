c1c29320e0460335ab0efa38096be8b879e44704
Amirametwally <amira.metwally56@gmail.com>
2024-03-05 22:49:33 +0200

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" function that queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ queries the Reddit API and prints the
+    titles of the first 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children'][:10]:
+                print(post['data']['title'])
+        else:
+            print(None)
+    else:
+        print(None)
