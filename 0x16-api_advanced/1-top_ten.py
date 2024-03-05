66ff92503ee87518142a81efdcdd660822edafa2
Amirametwally <amira.metwally56@gmail.com>
2024-03-06 00:32:52 +0200

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,18 +6,14 @@ import requests
 
 
 def top_ten(subreddit):
-    """ queries the Reddit API and prints the
-    titles of the first 10 hot posts"""
+    """ prints the titles of the first 10 hot posts"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children'][:10]:
-                print(post['data']['title'])
-        else:
-            print(None)
+        for i in range(10):
+            title = response.json()["data"]["children"][i]["data"]["title"]
+            print(title)
     else:
         print(None)
